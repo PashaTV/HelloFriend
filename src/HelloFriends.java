4ef8145eeea27e6922adc3fd336735294882b17c
PashaTV <graviticia@gmail/com>
2019-03-10 22:37:57 +0200

    Initial commit

---
+++ src/HelloFriends.java
@@ -0,0 +1,11 @@
+public class HelloFriends {
+    public static void main(String args[]) {
+        int var1; // объявляем переменную
+        int var2; // объявляем вторую переменную
+        var1 = 1000;
+        System.out.println (" Переменная var1 cодержит "  +var1);
+        var2 = var1/4;
+        System.out.println(" Переменная var2 содержит var1/4:" );
+        System.out.println(var2);
+    }
+    }
